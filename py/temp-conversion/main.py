d1b74deb275c8dd4081a9c12a1b2ebe152adc5e6
Ivan Kara <codexana133@gmail.com>
2019-09-23 18:55:08 +0700

    Done temp conversion python

--- py/temp-conversion/main.py
+++ py/temp-conversion/main.py
@@ -1,8 +1,16 @@
 # Input celcius.
-celcius = input('Suhu celcius: ')
+celcius = float(input('Suhu celcius: '))
 
 # Konversi ke kelvin.
 kelvin = celcius + 273
+        
+# Konversi ke Reamur.
+reamur = celcius * 4 / 5
 
-# Konversi ke reamur.
-reamur = 
+# Konversi ke Fahrenheit.
+fahrenheit = celcius * 9 / 5 + 32
+
+# Print hasil.
+print("Kelvin: " + str(kelvin))
+print("Reamur: " + str(reamur))
+print("Fahrenheit: " + str(fahrenheit))
