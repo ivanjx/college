26d0feb2297167e4dd0fd86a52f7eb0e53789dc2
Ivan Kara <codexana133@gmail.com>
2019-09-26 17:51:33 +0700

    Added coin stock feature

--- py/coin-calc/main.py
+++ py/coin-calc/main.py
@@ -1,7 +1,12 @@
 import json
 
 
-def calculate(cent):
+def calculate(
+  quarter_stock,
+  dime_stock,
+  nickle_stock,
+  penny_stock,
+  cent):
   # Note:
   # 1 quarter = 25 cents
   # 1 dime = 10 cents
@@ -10,30 +15,54 @@ def calculate(cent):
   result = {}
 
   # Calculating quarter.
-  result['quarter'] = cent // 25
+  quarter_count = cent // 25
+  quarter_stock_rem = max(quarter_stock - quarter_count, 0) # Cant be negative.
+  result['quarter'] = (quarter_stock - quarter_stock_rem)
   cent -= result['quarter'] * 25
 
   # Calculating dime.
-  result['dime'] = cent // 10
+  dime_count = cent // 10
+  dime_stock_rem = max(dime_stock - dime_count, 0) # Cant be negative.
+  result['dime'] = (dime_stock - dime_stock_rem)
   cent -= result['dime'] * 10
 
   # Calculating nickle.
-  result['nickle'] = cent // 5
+  nickle_count = cent // 5
+  nickle_stock_rem = max(nickle_stock - nickle_count, 0) # Cant be negative.
+  result['nickle'] = (nickle_stock - nickle_stock_rem)
   cent -= result['nickle'] * 5
 
   # Pennies.
-  result['penny'] = cent
+  penny_stock_rem = max(penny_stock - cent, 0) # Cant be negative.
+  result['penny'] = (penny_stock - penny_stock_rem)
+  cent -= result['penny']
+
+  # Check.
+  if cent > 0:
+    raise Exception('Insufficient coin stock')
 
   # Done.
   return result
 
 
 def main():
+  # Input coin stock.
+  quarter_stock = int(input('Quarter: '))
+  dime_stock = int(input('Dime: '))
+  nickle_stock = int(input('Nickle: '))
+  penny_stock = int(input('Penny: '))
+
   # Input cents.
   cent = int(input('Cents: '))
   
   # Calculate.
-  result = calculate(cent)
+  result = calculate(
+    quarter_stock,
+    dime_stock,
+    nickle_stock,
+    penny_stock,
+    cent
+  )
 
   # Done.
   print(json.dumps(result))
