e073d3cb1285cd2571eff36cb397179492977a40
Ivan Kara <codexana133@gmail.com>
2019-11-12 12:14:01 +0700

    Done matrix task

---
+++ java/task04_smt1/src/task04_smt1/Task04_smt1.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task04_smt1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author xanal
+ */
+public class Task04_smt1 {
+    static void print(int[][] matrix) {
+        for(int v = 0; v < matrix.length; ++v) {
+            for(int h = 0; h < matrix[v].length; ++h) {
+                System.out.print(matrix[v][h]);
+                System.out.print(" ");
+            }
+            
+            System.out.println();
+        }
+    }
+    
+    static int[][] add(int[][] m1, int[][] m2) {
+        int[][] result = new int[m1.length][m1[0].length];
+        
+        for(int v = 0; v < result.length; ++v) {
+            for(int h = 0; h < result[v].length; ++h) {
+                result[v][h] = m1[v][h] + m2[v][h];
+            }
+        }
+        
+        return result;
+    }
+    
+    static int[][] transpose(int[][] matrix) {
+        int srcV = matrix.length;
+        int srcH = matrix[0].length;
+        int[][] result = new int[srcH][srcV];
+        
+        for(int v = 0; v < srcV; ++v) {
+            for(int h = 0; h < srcH; ++h) {
+                result[h][v] = matrix[v][h];
+            }
+        }
+        
+        return result;
+    }
+    
+    static int[][] mul(int[][] m1, int[][] m2) {
+        int m1V = m1.length;
+        int m1H = m1[0].length;
+        int m2V = m2.length;
+        int m2H = m2[0].length;
+        
+        int[][] result = new int[m1V][m2H];
+        
+        for(int v = 0; v < result.length; ++v) {
+            for(int h = 0; h < result[v].length; ++h) {
+                result[v][h] = 0;
+                
+                for(int i = 0; i < m1V; ++i) {
+                    result[v][h] += m1[v][i] * m2[i][h];
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        int[][] A = new int[][] {
+            new int[] {1, 2, 3, 4},
+            new int[] {1, 2, 3, 4},
+            new int[] {1, 2, 3, 4},
+            new int[] {1, 2, 3, 4},
+        };
+        
+        int[][] B = new int[][] {
+            new int[] {1, 2, 3, 4},
+            new int[] {1, 2, 3, 4},
+            new int[] {1, 2, 3, 4},
+            new int[] {1, 2, 3, 4},
+        };
+        
+        int[][] C = new int[][] {
+            new int[] {1, 2, 3, 4},
+            new int[] {1, 2, 3, 4},
+            new int[] {1, 2, 3, 4},
+            new int[] {1, 2, 3, 4},
+        };
+        
+        System.out.println("A:");
+        print(A);
+        System.out.println();
+        
+        System.out.println("B:");        
+        print(B);
+        System.out.println();
+        
+        System.out.println("C:");        
+        print(C);
+        System.out.println();
+        
+        System.out.println("C+B:");
+        int[][] B_C = add(B, C);
+        print(B_C);
+        System.out.println();
+        
+        System.out.println("T(B+C):");
+        int[][] TBC = transpose(B_C);
+        print(TBC);
+        System.out.println();
+        
+        System.out.println("A*(B+C):");
+        int[][] AB_C = mul(A, TBC);
+        print(AB_C);
+        System.out.println();
+    }
+}
