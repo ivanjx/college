89706ecfb0ec01524cbb2dd48e49737cb88fe683
Ivan Kara <codexana133@gmail.com>
2019-11-26 07:02:16 +0700

    Done servlet sample

---
+++ java/test_servlet/src/WEB-INF/HelloServlet.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet
+{
+	// GET /
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+	throws 
+	ServletException,
+	IOException {
+		// Content type.
+		response.setContentType("text/html");
+
+		// Body.
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<h1>Test Servlet</h1>");
+		out.println("</body></html>");
+
+		// Done.
+		out.close();
+	}
+}
