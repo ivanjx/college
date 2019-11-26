0fc398cf6636577e0e27ee9280d5d7b90b37caa5
Ivan Kara <codexana133@gmail.com>
2019-11-26 07:24:02 +0700

    Added packaging

--- java/test_servlet/build.bat
+++ java/test_servlet/build.bat
@@ -4,3 +4,9 @@ javac -classpath src\WEB-INF\lib\servlet.jar src\WEB-INF\HelloServlet.java
 REM Moving to classes
 mkdir src\WEB-INF\classes
 move src\WEB-INF\HelloServlet.class src\WEB-INF\classes\HelloServlet.class
+
+REM Packaging
+cd src
+jar cvf test.war .
+cd ..
+move src\test.war test.war
