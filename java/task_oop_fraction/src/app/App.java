256d701129510ad9839c425cbbb030fde7690734
Ivan Kara <codexana133@gmail.com>
2019-12-15 19:00:27 +0700

    Changed launch config

--- java/task_oop_fraction/src/app/App.java
+++ java/task_oop_fraction/src/app/App.java
@@ -4,7 +4,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         Pecahan p1 = new Pecahan(4, 2);
         Pecahan p2 = new Pecahan(4, 2);
+
+
         Pecahan p3 = p1.bagi(p2);
+
+        
         System.out.println(p3.toStr());
     }
 }
