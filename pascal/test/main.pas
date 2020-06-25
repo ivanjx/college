8bdf78b9bf76f9d4048817fcde43bb17ed74fa62
Ivan Kara <codexana133@gmail.com>
2020-06-25 09:12:22 +0700

    Added function; Added if;

--- pascal/test/main.pas
+++ pascal/test/main.pas
@@ -1,16 +1,73 @@
 program main;
 
+procedure greetings;
 var
     name: String;
-
 begin
     WriteLn('Hello world!');
     Write('Your name: ');
     ReadLn(name);
     WriteLn('Hello ' + name);
+end;
+
+function add(num1: Integer; num2: Integer):Real;
+begin
+    add := num1 + num2;
+end;
+
+function sub(num1: Integer; num2: Integer):Real;
+begin
+    sub := num1 - num2;
+end;
+
+function divide(num1: Integer; num2: Integer):Real;
+begin
+    divide := num1 / num2;
+end;
+
+function mul(num1: Integer; num2: Integer):Real;
+begin
+    mul := num1 * num2;
+end;
+
+var
+    num1: Integer;
+    num2: Integer;
+    res: Real;
+    op: Char;
+
+begin
+    greetings();
+
+    Write('Enter number1: ');
+    ReadLn(num1);
+    Write('Enter number2: ');
+    ReadLn(num2);
+    Write('Operator (+-*/): ');
+    ReadLn(op);
+
+    if op = '+' then
+    begin
+        res := add(num1, num2);
+    end
+    else if op = '-' then
+    begin
+        res := sub(num1, num2);
+    end
+    else if op = '*' then
+    begin
+        res := mul(num1, num2);
+    end
+    else
+    begin
+        res := divide(num1, num2);
+    end;
+
+    Write('Result: ');
+    WriteLn(res);
 
     // End.
-    Write('Press any key to continue...');
+    Write('Press enter to exit...');
     ReadLn();
     WriteLn('Bye!');
 end.
