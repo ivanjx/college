46121dd530ae7d415296ce2b486d737c9fa973cb
Ivan Kara <codexana133@gmail.com>
2020-08-11 15:08:57 +0700

    WIP: uas2

---
+++ cs/Uas2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Uas2
+{
+    class Program
+    {
+        static void PrintTitle()
+        {
+            Console.WriteLine("                   CAFE FRIED CHICKEN IVAN                   ");
+        }
+
+        static void PrintBoldLine()
+        {
+            Console.WriteLine("=============================================================");
+        }
+
+        static void PrintMenuTitle()
+        {
+            
+            Console.WriteLine("No.  JENIS       HARGA       BANYAK      JUMLAH");
+            Console.WriteLine("     POTONGAN    SATUAN      BELI        HARGA");
+            Console.WriteLine("-------------------------------------------------------------");
+
+        }
+
+        static void PrintMenu()
+        {
+            PrintBoldLine();
+            PrintMenuTitle();
+
+            PrintBoldLine();
+        }
+
+        static void Main(string[] args)
+        {
+            PrintTitle();
+            PrintMenu();
+
+            
+        }
+    }
+}
