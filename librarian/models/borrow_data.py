8e5daaea26266484ae789db80efb1fe801845086
Ivan Kara <codexana133@gmail.com>
2020-10-15 16:44:46 +0700

    Added borrow data repository

--- librarian/models/borrow_data.py
+++ librarian/models/borrow_data.py
@@ -5,10 +5,10 @@ class borrow_data:
         self.nim = ""
         self.isbn = ""
         self.start_date = datetime.now()
-        self.end_date = datetime.now()
+        self.return_date = datetime.now()
 
     def is_borrowing(self):
         return self.start_date > datetime.now()
 
-    def is_late(self):
-        return self.end_date < datetime.now()
+    def is_returned(self):
+        return self.return_date != None
---
+++ librarian/repositories/borrow_data_repository.py
@@ -0,0 +1,29 @@
+class borrow_data_repository:
+    def __init__(self):
+        self.borrow_data_list = []
+
+    def create(self, borrow_data):
+        self.borrow_data_list.append(borrow_data)
+
+    def get_by_mhs_nim(self, nim):
+        return [x for x in self.borrow_data_list if x.nim == nim]
+
+    def get_by_book_isbn(self, isbn):
+        return [x for x in self.borrow_data_list if x.isbn == isbn]
+
+    def get_non_returned(self):
+        return [
+            x for x in self.borrow_data_list
+            if not x.is_returned()
+        ]
+
+    def get(self, nim, isbn):
+        for data in self.borrow_data_list:
+            if data.nim == nim and data.isbn == isbn:
+                return data
+
+        return None
+
+    def set_return_date(self, nim, isbn, date):
+        data = self.get(nim, isbn)
+        data.return_date = date
