581ec076b8ff5226f848dd73e709d4c8270948eb
Ivan Kara <codexana133@gmail.com>
2020-10-16 10:59:31 +0700

    Better function names

--- librarian/repositories/borrow_data_repository.py
+++ librarian/repositories/borrow_data_repository.py
@@ -2,20 +2,10 @@ class borrow_data_repository:
     def __init__(self):
         self.borrow_data_list = []
 
+
     def create(self, borrow_data):
         self.borrow_data_list.append(borrow_data)
 
-    def get_by_mhs_nim(self, nim):
-        return [x for x in self.borrow_data_list if x.nim == nim]
-
-    def get_by_book_isbn(self, isbn):
-        return [x for x in self.borrow_data_list if x.isbn == isbn]
-
-    def get_non_returned(self):
-        return [
-            x for x in self.borrow_data_list
-            if not x.is_returned
-        ]
 
     def get(self, nim, isbn):
         for data in self.borrow_data_list:
@@ -24,16 +14,34 @@ class borrow_data_repository:
 
         return None
 
+
+    def list_by_mhs_nim(self, nim):
+        return [x for x in self.borrow_data_list if x.nim == nim]
+
+
+    def list_by_book_isbn(self, isbn):
+        return [x for x in self.borrow_data_list if x.isbn == isbn]
+
+
+    def list_non_returned(self):
+        return [
+            x for x in self.borrow_data_list
+            if not x.is_returned
+        ]
+
+
     def set_return_date(self, nim, isbn, date):
         data = self.get(nim, isbn)
         data.is_returned = True
         data.return_date = date
+
 
     def delete_by_nim(self, nim):
         self.borrow_data_list = [
             x for x in self.borrow_data_list
             if x.nim != nim
         ]
+
 
     def delete_by_isbn(self, isbn):
         self.borrow_data_list = [
