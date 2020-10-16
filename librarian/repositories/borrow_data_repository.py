e99794151d1fa8315f97af91d81d9df580cfeb4a
Ivan Kara <codexana133@gmail.com>
2020-10-16 15:42:02 +0700

    Added borrow data persistence

--- librarian/repositories/borrow_data_repository.py
+++ librarian/repositories/borrow_data_repository.py
@@ -1,4 +1,61 @@
+import os.path
+import json
+from datetime import datetime
+from models.borrow_data import borrow_data
+
+
 class borrow_data_repository:
+    NAME = "db/borrow.json"
+
+
+    def load(self):
+        self.borrow_data_list = []
+
+        # Check if file exists.
+        if not os.path.isfile(borrow_data_repository.NAME):
+            return
+
+        # Reading file.
+        with open(borrow_data_repository.NAME, "r") as infs:
+            raw = json.load(infs)
+
+            if not raw["data"] or not isinstance(raw["data"], list):
+                # Invalid file content.
+                infs.close()
+                os.unlink(borrow_data_repository.NAME)
+            else:
+                # Deserializing.
+                for r in raw["data"]:
+                    b = borrow_data()
+                    b.nim = r["nim"]
+                    b.isbn = r["isbn"]
+                    b.start_date = datetime.fromtimestamp(r["start_date"])
+                    b.is_returned = r["is_returned"]
+                    b.return_date = datetime.fromtimestamp(r["return_date"])
+                    self.borrow_data_list.append(b)
+
+
+    def save(self):
+        if not os.path.isdir("db"): os.mkdir("db")
+
+        with open(borrow_data_repository.NAME, "w+") as outfs:
+            # Serializing.
+            raw = {}
+            raw["data"] = []
+
+            for b in self.borrow_data_list:
+                r = {}
+                r["nim"] = b.nim
+                r["isbn"] = b.isbn
+                r["start_date"] = b.start_date.timestamp()
+                r["is_returned"] = b.is_returned 
+                r["return_date"] = b.return_date.timestamp()
+                raw["data"].append(r)
+
+            # Saving.
+            json.dump(raw, outfs)
+
+
     def __init__(self):
         self.borrow_data_list = []
 
