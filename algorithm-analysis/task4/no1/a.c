c4a3a0f68cd8967cdeb2c3349ce4e9ff7d7c9acb
Ivan Kara <codexana133@gmail.com>
2020-10-25 12:35:40 +0700

    Done no 1

---
+++ algorithm-analysis/task4/no1/a.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    
+    if (n > 50)
+    {
+        n -= 25;
+    }
+
+    n += 10;
+    printf("%d\n", n);
+    getch();
+
+    return 0;
+}
