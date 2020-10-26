5aa0197230eab00b08d3ebacb29c533213091cad
Ivan Kara <codexana133@gmail.com>
2020-10-26 17:40:39 +0700

    Init

---
+++ internet-programming-uts1/src/WEB-INF/MainServlet.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class MainServlet extends HttpServlet
+{
+	// GET /
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+	throws 
+	ServletException,
+	IOException {
+		// Content type.
+		response.setContentType("application/json");
+
+		// Body.
+		PrintWriter out = response.getWriter();
+		out.println("{\"message\": \"Hello world!\"}");
+
+		// Done.
+		out.close();
+	}
+}
