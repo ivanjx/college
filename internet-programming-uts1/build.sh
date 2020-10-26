eb8cbaaea34da138d2f0b3cf5d9e6a217f081e05
Ivan Kara <codexana133@gmail.com>
2020-10-26 19:13:22 +0700

    Changed build script to bash

---
+++ internet-programming-uts1/build.sh
@@ -0,0 +1,22 @@
+mkdir bin
+mkdir bin/WEB-INF
+mkdir bin/WEB-INF/classes
+mkdir bin/WEB-INF/lib
+
+echo Compiling
+javac -classpath \
+  ".:lib/servlet.jar:lib/jdbc.jar" \
+  -d bin/WEB-INF/classes \
+  src/WEB-INF/MainServlet.java
+
+cp -R src/* bin
+find ./bin -type f -name '*.java' -exec rm {} +
+
+echo Copying lib
+cp -R lib/* bin/WEB-INF/lib
+
+echo Packaging
+cd bin
+jar cvf build.war .
+mv build.war ../internet-programming-uts1.war
+rm -rf bin
