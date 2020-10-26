d08300260f02d74a1b177071d131e395ad195fcf
Ivan Kara <codexana133@gmail.com>
2020-10-27 05:33:56 +0700

    Done book category repository

--- internet-programming-uts1/src/logic/repositories/BookCategoryRepository.java
+++ internet-programming-uts1/src/logic/repositories/BookCategoryRepository.java
@@ -2,9 +2,7 @@ package logic.repositories;
 
 import logic.models.BookCategory;
 import java.sql.*;
-import java.util.List;
-
-import com.mysql.cj.protocol.Resultset;
+import java.util.*;
 
 public class BookCategoryRepository implements IBookCategoryRepository 
 {
@@ -16,7 +14,7 @@ public class BookCategoryRepository implements IBookCategoryRepository
         DB db = new DB();
         Statement statement = db.getStatement();
         ResultSet rs = statement.executeQuery("select * from tblBuku");
-        List<BookCategory> result = new List<BookCategory>();
+        List<BookCategory> result = new ArrayList<BookCategory>();
         
         while (rs.next())
         {
@@ -26,13 +24,26 @@ public class BookCategoryRepository implements IBookCategoryRepository
             result.add(cat);
         }
 
-        return result.toArray();
+        return result.toArray(new BookCategory[0]);
     }
 
     @Override
-    public BookCategory get(int id) 
+    public BookCategory get(int id)
+    throws SQLException 
     {
-        // TODO Auto-generated method stub
+        DB db = new DB();
+        PreparedStatement statement = db.prepareStatement("select * from tblBuku where id = ?");
+        statement.setInt(1, id);
+        ResultSet rs = statement.executeQuery();
+
+        if (rs.next())
+        {
+            BookCategory cat = new BookCategory();
+            cat.setId(rs.getInt(1));
+            cat.setName(rs.getString(2));
+            return cat;
+        }
+
         return null;
     }
 
