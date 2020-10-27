491e712c2abcf21ee8f82945b8cd3ddf4f63d5f1
Ivan Kara <codexana133@gmail.com>
2020-10-27 14:48:27 +0700

    Restart tomcat while deploying

--- internet-programming-uts1/deploy.sh
+++ internet-programming-uts1/deploy.sh
@@ -1,3 +1,4 @@
 sudo rm /usr/share/apache-tomcat-9.0.39/webapps/internet-programming-uts1.war
-sleep 3
 sudo cp internet-programming-uts1.war /usr/share/apache-tomcat-9.0.39/webapps
+sudo /usr/share/apache-tomcat-9.0.39/bin/shutdown.sh
+sudo /usr/share/apache-tomcat-9.0.39/bin/startup.sh
