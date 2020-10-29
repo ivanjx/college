8097fa9ef5cdfcdecd9bcc0f76431c8fbe3ce5b5
Ivan Kara <codexana133@gmail.com>
2020-10-29 12:18:44 +0700

    Added missing annotations

--- internet-programming-uts1/src/logic/controllers/buku/BukuDeleteServlet.java
+++ internet-programming-uts1/src/logic/controllers/buku/BukuDeleteServlet.java
@@ -3,12 +3,14 @@ package logic.controllers.buku;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import logic.DI;
 
+@WebServlet("/buku/delete")
 public class BukuDeleteServlet extends HttpServlet
 {
     private static final long serialVersionUID = 1L;
--- internet-programming-uts1/src/logic/controllers/buku/BukuUpdateServlet.java
+++ internet-programming-uts1/src/logic/controllers/buku/BukuUpdateServlet.java
@@ -3,6 +3,7 @@ package logic.controllers.buku;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import logic.DI;
 import logic.models.Book;
 
+@WebServlet("/buku/update")
 public class BukuUpdateServlet extends HttpServlet 
 {
     private static final long serialVersionUID = 1L;
