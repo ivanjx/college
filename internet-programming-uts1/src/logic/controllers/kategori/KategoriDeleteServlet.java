60d3588c4bb5f29f5e366ac346b9b871d49f9721
Ivan Kara <codexana133@gmail.com>
2020-10-29 13:36:11 +0700

    Changed delete method

--- internet-programming-uts1/src/logic/controllers/kategori/KategoriDeleteServlet.java
+++ internet-programming-uts1/src/logic/controllers/kategori/KategoriDeleteServlet.java
@@ -15,7 +15,7 @@ public class KategoriDeleteServlet extends HttpServlet
 {
     private static final long serialVersionUID = 1L;
     
-    public void doPost(HttpServletRequest request, HttpServletResponse response) 
+    public void doGet(HttpServletRequest request, HttpServletResponse response) 
     throws ServletException, IOException
     {
         try
