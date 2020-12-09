36a9bb136de8192681d0f49f81c0199846f40a69
Ivan Kara <codexana133@gmail.com>
2020-12-09 23:43:35 +0700

    Done task9

---
+++ internet-programming/task9/build.sh
@@ -0,0 +1,18 @@
+echo Preparing
+mkdir bin
+mkdir bin/WEB-INF
+mkdir bin/WEB-INF/classes
+mkdir bin/WEB-INF/lib
+
+echo Compiling
+cp -R src/* bin
+
+echo Copying lib
+cp -R lib/* bin/WEB-INF/lib
+
+echo Packaging
+cd bin
+jar cvf build.war .
+mv build.war ../task9.war
+cd ..
+rm -rf bin
---
+++ internet-programming/task9/src/index.jsp
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Latihan Website</title>
+    </head>
+    <body>
+        <h1>Menampilkan tanggal dan waktu sekarang</h1>
+        <%= new java.util.Date() %>
+    </body>
+</html>
