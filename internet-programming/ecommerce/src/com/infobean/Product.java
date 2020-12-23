bc71592791fe6b976997524cf697e6e690ae2bfc
Ivan Kara <codexana133@gmail.com>
2020-12-23 12:57:49 +0700

    Added models

--- internet-programming/ecommerce/src/com/infobean/Product.java
+++ internet-programming/ecommerce/src/com/infobean/Product.java
@@ -1,5 +1,67 @@
 package com.infobean;
 
 public class Product {
-    
+    private String id;
+    private String sku;
+    private String name;
+    private String brand;
+    private String description;
+    private Category category;
+    private String image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSKU() {
+        return sku;
+    }
+
+    public void setSKU(String sku) {
+        this.sku = sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
---
+++ internet-programming/ecommerce/src/com/infobean/Promotion.java
@@ -0,0 +1,22 @@
+package com.infobean;
+
+public class Promotion {
+    private String id;
+    private Product product;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
