718c86e2db3da13c2316aaceb477596493426932
Ivan Kara <codexana133@gmail.com>
2021-05-03 00:22:30 +0700

    Added question activity

--- Covid19SelfCheck/app/src/main/java/com/ivanrio/covid19selfcheck/MainActivity.java
+++ Covid19SelfCheck/app/src/main/java/com/ivanrio/covid19selfcheck/MainActivity.java
@@ -3,26 +3,38 @@ package com.ivanrio.covid19selfcheck;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.ivanrio.covid19selfcheck.databinding.ActivityMainBinding;
 import com.ivanrio.covid19selfcheck.logic.IEventListener;
 import com.ivanrio.covid19selfcheck.logic.MainViewModel;
+import com.ivanrio.covid19selfcheck.logic.VMLocator;
 
 public class MainActivity extends AppCompatActivity {
-    private MainViewModel _vm;
     private ActivityMainBinding _binding;
 
     public MainActivity() {
         ResHelper.inject(this);
-        _vm = new MainViewModel();
-        _vm.addCloseRequestedListener(() -> finishAndRemoveTask()); // As long as the interface only has one method.
+        VMLocator.mainVM.addCloseRequestedListener(() -> finishAndRemoveTask()); // As long as the interface only has one method.
+        VMLocator.mainVM.addStartedListener(() -> startQuestionActivity());
+        VMLocator.mainVM.addConclusionMadeListener(() -> startConclusionActivity());
+    }
+
+    private void startQuestionActivity() {
+        Intent intent = new Intent(this, QuestionActivity.class);
+        startActivity(intent);
+    }
+
+    private void startConclusionActivity() {
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        _binding.setVM(_vm);
+        _binding.setVM(VMLocator.mainVM);
     }
 }
--- Covid19SelfCheck/app/src/main/java/com/ivanrio/covid19selfcheck/QuestionActivity.java
+++ Covid19SelfCheck/app/src/main/java/com/ivanrio/covid19selfcheck/QuestionActivity.java
@@ -1,14 +1,22 @@
 package com.ivanrio.covid19selfcheck;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.ivanrio.covid19selfcheck.databinding.ActivityMainBinding;
+import com.ivanrio.covid19selfcheck.databinding.ActivityQuestionBinding;
+import com.ivanrio.covid19selfcheck.logic.MainViewModel;
+import com.ivanrio.covid19selfcheck.logic.VMLocator;
+
 public class QuestionActivity extends AppCompatActivity {
+    private ActivityQuestionBinding _binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question);
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_question);
+        _binding.setVM(VMLocator.mainVM);
     }
 }
